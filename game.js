b59cfd67d16005e89da4d55f81321ea9792cce5b
aaryan <aaryanpanda04@gmail.com>
2023-10-13 17:06:47 +0530

    Included comments and fixed popup function

--- game.js
+++ game.js
@@ -3,6 +3,9 @@ var randomImage = document.getElementById("random-image");
 var score1 = 0;
 var score2 = 0;
 
+
+
+//function to make buttons dynamic
 document.addEventListener('click', (event) => {
     if (event.target.id == "rock") {
         setUserChoiceImage('./assets/rock-hand.png');
@@ -21,11 +24,16 @@ function getRandomNum(max) {
 
 const ImageArray = ['./assets/rock-hand.png', './assets/paper-hand.png', './assets/scissors-hand.png'];
 
+
+
+//Function for computer image
 function displayRandomImage() {
     let randomImageSrc = getRandomNum(2);
     randomImage.innerHTML = `<img src="${ImageArray[randomImageSrc]}" alt="Random Image">`;
 }
 
+
+//Function for user image
 function setUserChoiceImage(imageSrc) {
     let userImage = document.createElement('img');
     userImage.src = imageSrc;
@@ -33,6 +41,8 @@ function setUserChoiceImage(imageSrc) {
     userChoice.appendChild(userImage);
 }
 
+
+//Function to Compare score
 function compareScore() {
     let pcImageSrc = randomImage.firstElementChild.src;
     let userImageSrc = userChoice.firstElementChild.src;
@@ -56,6 +66,8 @@ function compareScore() {
     }
 }
 
+
+//Function to update score
 function updateScore() {
     let userScoreDisplay = document.getElementById("user-score");
     let pcScoreDisplay = document.getElementById("pc-score");
@@ -63,19 +75,31 @@ function updateScore() {
     pcScoreDisplay.innerHTML = score2;
 }
 
+
+//Function to display popup
+
+var win = document.getElementById("you-win")
+var lose = document.getElementById("you-lose")
+
 function popup(result) {
     if (score1 == 5 || score2 == 5) {
         result.style.display = "block";
     }
 }
 
+
+//Function Reset Game
 function resetGame() {
     if (score1 === 5 || score2 === 5) {
         window.location.reload();
     }
 }
 
+
+//Function to Play Again
 var playAgain = document.getElementById("play-again");
 playAgain.addEventListener('click', () => {
     window.location.reload();
 });
+
+
